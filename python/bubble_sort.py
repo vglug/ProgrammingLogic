48a0f6ed3d81fd04b48297576a5d573ce02f5556
dilip0207 <54951927+dilip0207@users.noreply.github.com>
2020-10-30 11:23:16 +0530

    Update bubble_sort.py

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -21,7 +21,7 @@ def bubbleSort(arr):
 arr = [64, 34, 25, 12, 22, 11, 90] 
   
 bubbleSort(arr) 
-  
+#printing sorted array  
 print ("Sorted array is:") 
 for i in range(len(arr)): 
     print ("%d" %arr[i]),  
