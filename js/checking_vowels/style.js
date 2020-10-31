7967fe226a3273c7552ac6555930000ea021d633
Keerthana <keerthanaravichandran96@gmail.com>
2020-10-31 13:18:37 +0530

    vowels checking function modified

--- js/checking_vowels/style.js
+++ js/checking_vowels/style.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#submit').on('click', function(){
-    var user_input = $('#input_letter').val();
-    var vowels = ['a','e','i','o','u','A','E','I','O','U'];
+    var user_input = $('#input_letter').val().toUpperCase();
+    var vowels = ['A','E','I','O','U'];
     if (vowels.includes(user_input)){
         $('.result').html("Yes");
         console.log("Yes");
