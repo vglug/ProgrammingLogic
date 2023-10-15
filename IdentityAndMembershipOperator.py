e844e1ae6162e69468546159881cab69178b1297
Vikkibala007 <131947390+Vikkibala007@users.noreply.github.com>
2023-10-15 15:38:47 +0530

    IdentityAndMembershipOperator.py
    Identity Operators & Membership Operators(hacktoberfest)

---
+++ IdentityAndMembershipOperator.py
@@ -0,0 +1,11 @@
+#IDENTITY OPERATOR 
+x = [1, 2, 3]
+y = x  
+
+print(x is y)     
+print(x is not y)
+#MEMBERSHIP OPERATOR 
+numbers = [1, 2, 3, 4, 5]
+
+print(3 in numbers)     
+print(6 not in numbers) 
