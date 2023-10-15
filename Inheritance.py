739309ad479898da760f27797b28ee485312354a
Vikkibala007 <131947390+Vikkibala007@users.noreply.github.com>
2023-10-15 13:09:47 +0530

    Create Inheritance.py
    Hacktofest

---
+++ Inheritance.py
@@ -0,0 +1,46 @@
+class Person(object):
+ 
+
+   
+
+    def __init__(self, name):
+
+        self.name = name
+ 
+
+   
+
+    def getName(self):
+
+        return self.name
+ 
+
+    
+
+    def isEmployee(self):
+
+        return False
+ 
+ 
+
+
+class Employee(Person):
+ 
+
+    # Here we return true
+
+    def isEmployee(self):
+
+        return True
+ 
+ 
+
+
+emp = Person("Geek1")  # An Object of Person
+
+print(emp.getName(), emp.isEmployee())
+ 
+
+emp = Employee("Geek2")  # An Object of Employee
+
+print(emp.getName(), emp.isEmployee
