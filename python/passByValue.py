6b617fe12658728baf909ccb5c2752ca1d637882
69thilip-M <thilipmuruganprema649@gmail.com>
2023-10-15 13:18:52 +0530

    update

--- python/passByValue.py
+++
@@ -1,9 +0,0 @@
-""" Pass By Value """
-def add100(num):
-    num+=100
-    print("Changed value: "+str(num))
-
-print("-------Pass By value-------")
-num = int(input("Enter the number: "))
-add100(num)
-print("Input after the Function calls: "+str(num))
