eb2cd5013d6786b203e851a45d207330ddc7c526
dkesavapriya <dkesavapriya@gmail.com>
2023-10-15 12:06:06 +0530

    program for counting element in list

---
+++ python/countInList.py
@@ -0,0 +1,8 @@
+
+# Sample list
+list1 = [1, 2, 3, 4, 1, 2, 1, 1]
+
+# Count occurrences of a specific element, for example, counting occurrences of 1
+count_of_element = list1.count(1)
+
+print("Count of 1 in the list:", count_of_element)
