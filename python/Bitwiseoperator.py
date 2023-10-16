9f3e2bfb6bff18f30b1ec774c4b8cc509b944a0e
Harshaharika7 <138356696+Harshaharika7@users.noreply.github.com>
2023-10-16 16:58:01 +0530

    Create Bitwiseoperator.py

---
+++ python/Bitwiseoperator.py
@@ -0,0 +1,5 @@
+a = 5  # Binary: 0101
+left_shifted = a << 2  # Left shift by 2 positions
+right_shifted = a >> 1  # Right shift by 1 position
+print(left_shifted)   #Binary: 10100
+print(right_shifted)  #Binary: 0010
