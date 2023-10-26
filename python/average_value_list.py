f464e748f2923db952044cc6bfc836898b396763
Deepakraghavan01 <57013229+Deepakraghavan01@users.noreply.github.com>
2023-10-26 22:18:30 +0530

    Delete python/average_value_list.py
    Deleted unwanted creation

--- python/average_value_list.py
+++
@@ -1,8 +0,0 @@
-def nearest_to_average(lst):
-    avg = sum(lst) / len(lst)
-    return min(lst, key=lambda x: abs(x - avg))
-
-# Example usage
-my_list = [1, 5, 9, 2, 3, 7]
-result = nearest_to_average(my_list)
-print(f"The element nearest to the average is: {result}")
