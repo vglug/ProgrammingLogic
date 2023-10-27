949ec339a38df1c82547e7e0aeec99793771eb3f
Sheelavijayaranga <128619068+Sheelavijayaranga@users.noreply.github.com>
2023-10-27 19:03:03 +0530

    Create remove.py

---
+++ python/remove.py
@@ -0,0 +1,9 @@
+# Initialization string
+string = "Welcome To SparkByExamples Tutorial"
+print("Original string:", string)
+
+# Using replace() method
+# Remove substring from string 
+substr_to_remove = "Tutorial"
+result = string.replace(substr_to_remove, "")
+print("The string after removing substring:",result)
